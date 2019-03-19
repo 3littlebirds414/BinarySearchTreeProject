029de14167a0a1b0e7ba3041298d416aeca6a945
Natalie Zavada <3littlebirds414@gmail.com>
2019-03-19 11:17:31 -0500

    Built out my Class for Node

--- BinarySearchTree/BinarySearchTree/Node.cs
+++ BinarySearchTree/BinarySearchTree/Node.cs
@@ -8,5 +8,68 @@ namespace BinarySearchTree
 {
     class Node
     {
+        public int value;
+        public Node left;
+        public Node right;
+        public Node parent;
+
+        public Node (int initial)
+        {
+            
+            value = initial;
+            left = null;
+            right = null;
+        }
+
+        public int initial
+        {
+            get
+            {
+                return initial;
+            }
+            set
+            {
+                initial = value;
+            }
+        }
+
+        public Node Left
+        {
+            get
+            {
+                return left;
+            }
+            set
+            {
+                left = value;
+            }
+        }
+        public Node Right
+        {
+            get
+            {
+                return right;
+            }
+            set
+            {
+                right = value;
+            }
+        }
+
+        public Node Parent
+        {
+            get
+            {
+                return parent;
+            }
+            set
+            {
+                parent = value;
+            }
+        }
+
+
+
+
     }
 }
